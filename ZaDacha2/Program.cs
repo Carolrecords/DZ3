c35defc5de76d3503d427ddfb84f5bc25067cd2c
korolev_as <antonykas@mail.ru>
2022-11-20 21:15:43 +0300

    git init

---
+++ ZaDacha2/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек 
+//и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine ("Введите значен A1");
+var x1 = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine ("Введите значен A2");
+var y1 = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine ("Введите значен A3");
+var z1 = Convert.ToInt32(Console.ReadLine ());
+
+Console.WriteLine ("Введите значен B1");
+var x2 = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine ("Введите значен B2");
+var y2 = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine ("Введите значен B3");
+var z2 = Convert.ToInt32(Console.ReadLine ());
+
+
+var result = Math.Sqrt(Math.Pow ((x2 - x1), 2) + Math.Pow ((y2 - y1), 2) + Math.Pow ((z2 - z1), 2));
+
+Console.WriteLine (result);
---
+++ ZaDacha3/Program.cs
@@ -0,0 +1,10 @@
+﻿// принимает на вход N число
+//и выдает таблицу кубов чисел от 1 до N
+
+Console.WriteLine("Введите число ");
+var num = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= num; i++)
+{
+    Console.WriteLine(Math.Pow(i,3));
+}
